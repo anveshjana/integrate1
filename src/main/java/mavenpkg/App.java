95bda00384136a81ef3c8a616b4508d550d7f47c
EC2 Default User <ec2-user@ip-172-31-41-183.ap-south-1.compute.internal>
2023-01-25 04:19:04 +0000

    committing from local jar folder

---
+++ src/main/java/mavenpkg/App.java
@@ -0,0 +1,13 @@
+package mavenpkg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
